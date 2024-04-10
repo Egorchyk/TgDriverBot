2531b83159e104e78f647d597c08f322f9cab835
Egorchyk <xaritonov.egorchyk@gmail.com>
2024-04-10 22:10:08 +0300

    Initial commit

---
+++ src/main/java/com/example/tgdriverbot/service/TelegramBot.java
@@ -0,0 +1,33 @@
+package com.example.tgdriverbot.service;
+
+import com.example.tgdriverbot.config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("SUCCES!!!");
+    }
+}
---
+++ src/test/java/com/example/tgdriverbot/TgDriverBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgdriverbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgDriverBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
